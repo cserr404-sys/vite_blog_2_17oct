5a44070114cb0077de1a04e10e2ff3472d30537b
cserr404 <cserr404@gmail.com>
2025-10-16 21:24:43 +0000

    css2

--- blogs-bin/next.config.ts
+++ blogs-bin/next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    esmExternals: "loose",
-    serverComponentsExternalPackages: ["@zerodev/permissions", "@zerodev/sdk"],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
